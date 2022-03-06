a2025e489ac48456cd5e2880fefbc272a57c59e4
ProxymeWeb <proxymegame@gmail.com>
2022-03-06 13:51:07 +0100

    START

---
+++ BaseBall/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿
+namespace BaseBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labscore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.balle = new System.Windows.Forms.PictureBox();
+            this.main = new System.Windows.Forms.PictureBox();
+            this.decompte = new System.Windows.Forms.Label();
+            this.EZbtn = new System.Windows.Forms.RadioButton();
+            this.MDbtn = new System.Windows.Forms.RadioButton();
+            this.HDbtn = new System.Windows.Forms.RadioButton();
+            this.groupBoxDiff = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.nbballe = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.balle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.main)).BeginInit();
+            this.groupBoxDiff.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labscore
+            // 
+            this.labscore.AutoSize = true;
+            this.labscore.Font = new System.Drawing.Font("Stencil", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labscore.Location = new System.Drawing.Point(259, 5);
+            this.labscore.Name = "labscore";
+            this.labscore.Size = new System.Drawing.Size(44, 47);
+            this.labscore.TabIndex = 1;
+            this.labscore.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(88, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 47);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SCORE :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(408, 545);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(248, 49);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            this.btnStart.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnStart_KeyDown);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.balle);
+            this.panel1.Controls.Add(this.main);
+            this.panel1.Controls.Add(this.decompte);
+            this.panel1.Location = new System.Drawing.Point(23, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(360, 570);
+            this.panel1.TabIndex = 5;
+            // 
+            // balle
+            // 
+            this.balle.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("balle.BackgroundImage")));
+            this.balle.Location = new System.Drawing.Point(175, 73);
+            this.balle.Name = "balle";
+            this.balle.Size = new System.Drawing.Size(10, 10);
+            this.balle.TabIndex = 6;
+            this.balle.TabStop = false;
+            this.balle.Click += new System.EventHandler(this.balle_Click);
+            // 
+            // main
+            // 
+            this.main.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.main.Location = new System.Drawing.Point(146, 555);
+            this.main.Name = "main";
+            this.main.Size = new System.Drawing.Size(63, 202);
+            this.main.TabIndex = 3;
+            this.main.TabStop = false;
+            this.main.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.main_PreviewKeyDown);
+            // 
+            // decompte
+            // 
+            this.decompte.AutoSize = true;
+            this.decompte.BackColor = System.Drawing.Color.Transparent;
+            this.decompte.Font = new System.Drawing.Font("Microsoft Sans Serif", 1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.decompte.Location = new System.Drawing.Point(128, 178);
+            this.decompte.Name = "decompte";
+            this.decompte.Size = new System.Drawing.Size(3, 2);
+            this.decompte.TabIndex = 5;
+            this.decompte.Text = "3";
+            // 
+            // EZbtn
+            // 
+            this.EZbtn.AutoSize = true;
+            this.EZbtn.Location = new System.Drawing.Point(13, 33);
+            this.EZbtn.Name = "EZbtn";
+            this.EZbtn.Size = new System.Drawing.Size(60, 17);
+            this.EZbtn.TabIndex = 6;
+            this.EZbtn.TabStop = true;
+            this.EZbtn.Text = "Facile";
+            this.EZbtn.UseVisualStyleBackColor = true;
+            // 
+            // MDbtn
+            // 
+            this.MDbtn.AutoSize = true;
+            this.MDbtn.Checked = true;
+            this.MDbtn.Location = new System.Drawing.Point(84, 34);
+            this.MDbtn.Name = "MDbtn";
+            this.MDbtn.Size = new System.Drawing.Size(61, 17);
+            this.MDbtn.TabIndex = 7;
+            this.MDbtn.TabStop = true;
+            this.MDbtn.Text = "Moyen";
+            this.MDbtn.UseVisualStyleBackColor = true;
+            // 
+            // HDbtn
+            // 
+            this.HDbtn.AutoSize = true;
+            this.HDbtn.Location = new System.Drawing.Point(159, 35);
+            this.HDbtn.Name = "HDbtn";
+            this.HDbtn.Size = new System.Drawing.Size(74, 17);
+            this.HDbtn.TabIndex = 8;
+            this.HDbtn.TabStop = true;
+            this.HDbtn.Text = "Difficile";
+            this.HDbtn.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxDiff
+            // 
+            this.groupBoxDiff.BackColor = System.Drawing.Color.Beige;
+            this.groupBoxDiff.Controls.Add(this.EZbtn);
+            this.groupBoxDiff.Controls.Add(this.HDbtn);
+            this.groupBoxDiff.Controls.Add(this.MDbtn);
+            this.groupBoxDiff.Font = new System.Drawing.Font("Stencil", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxDiff.Location = new System.Drawing.Point(408, 464);
+            this.groupBoxDiff.Name = "groupBoxDiff";
+            this.groupBoxDiff.Size = new System.Drawing.Size(248, 75);
+            this.groupBoxDiff.TabIndex = 9;
+            this.groupBoxDiff.TabStop = false;
+            this.groupBoxDiff.Text = "Difficulté";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(408, 184);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 290);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(408, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(246, 155);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // nbballe
+            // 
+            this.nbballe.AutoSize = true;
+            this.nbballe.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.nbballe.Font = new System.Drawing.Font("Stencil", 18.25F);
+            this.nbballe.ForeColor = System.Drawing.Color.Black;
+            this.nbballe.Location = new System.Drawing.Point(602, 133);
+            this.nbballe.Margin = new System.Windows.Forms.Padding(0);
+            this.nbballe.Name = "nbballe";
+            this.nbballe.Padding = new System.Windows.Forms.Padding(0, 3, 0, 5);
+            this.nbballe.Size = new System.Drawing.Size(41, 38);
+            this.nbballe.TabIndex = 12;
+            this.nbballe.Text = "10";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Beige;
+            this.groupBox1.Location = new System.Drawing.Point(408, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 573);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(674, 619);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nbballe);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.labscore);
+            this.Controls.Add(this.groupBoxDiff);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "BaseBall";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.balle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.main)).EndInit();
+            this.groupBoxDiff.ResumeLayout(false);
+            this.groupBoxDiff.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labscore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox main;
+        private System.Windows.Forms.Label decompte;
+        private System.Windows.Forms.PictureBox balle;
+        private System.Windows.Forms.RadioButton EZbtn;
+        private System.Windows.Forms.RadioButton MDbtn;
+        private System.Windows.Forms.RadioButton HDbtn;
+        private System.Windows.Forms.GroupBox groupBoxDiff;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label nbballe;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ BaseBall/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace BaseBall
+{
+    public partial class Form1 : Form
+    {
+        int vitessemain = 5; //diff moyenne
+        double vitesseballe = 0.2; // dif moyenne
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            int point = 0;
+            labscore.Text = "0";
+
+
+            if (EZbtn.Checked == true) //dif EAZY
+            {
+                vitessemain = 12;
+                vitesseballe = 0.1;
+
+            }else if(HDbtn.Checked == true) //diff HARD
+             {
+                vitessemain = 5;
+                vitesseballe = 0.5;
+            }
+
+
+
+
+
+            Random aleatoire = new Random();
+            groupBoxDiff.Enabled = false; //Empeche l'utilisateur de changer de focus pendant la partie
+
+            for (int i = 10; i > 0; i--)
+            {
+                nbballe.Text = (i - 1).ToString();
+                bool touche = false;
+
+                int multiplicateur = aleatoire.Next(3, 20); //
+                int xballe = aleatoire.Next(-10, 10);    //le -10 fais que y deviens negatif 
+                int acceleration = Convert.ToInt32(Math.Sqrt(xballe * xballe));
+                int yballe = acceleration * multiplicateur;   // permet a ce que y soit depandant de x mais soit jamais dans le negatif qui ferait partir la balle au dessus
+
+                balle.Location = new Point(175, 74);
+
+
+                pictureBox1.Image = Image.FromFile("../../GIF/giphy.gif");
+                pictureBox1.Visible = true;// GIF ANIMATION 
+                await Task.Delay(4700);
+                pictureBox1.Visible = false;
+
+                if (xballe != 0)
+                {
+
+                    while (balle.Top < 570)
+                    {
+                        balle.Top = balle.Top + Convert.ToInt32(yballe * vitesseballe);
+                        balle.Left = balle.Left + Convert.ToInt32(xballe * vitesseballe);
+                        await Task.Delay(1);
+
+                        if (balle.Top > main.Top && (balle.Left > main.Left && balle.Left < main.Left + 65))
+                        {
+                            touche = true;
+                            labscore.Text = point.ToString();
+                        }
+
+
+                    }
+
+                    if (touche == true)
+                    {
+                        point++;
+                    }
+                }
+                else
+                {
+                    pictureBox1.Image = Image.FromFile("../../GIF/fail.gif");   // C:/Users/maxim/Desktop/BaseBall
+                    pictureBox1.Visible = true;
+                    await Task.Delay(1700);
+                    pictureBox1.Visible = false;
+
+                }
+                await Task.Delay(2000);
+
+            }
+
+            groupBoxDiff.Enabled = Enabled;
+            if (point > 5)
+            {
+                pictureBox1.Visible = true;
+                pictureBox1.Image = Image.FromFile("../../GIF/good.gif");
+            }
+            else
+            {
+                pictureBox1.Visible = true;
+                pictureBox1.Image = Image.FromFile("../../GIF/bad.gif");
+            }
+
+        }
+        
+        private void groupBox1_MouseHover(object sender, EventArgs e)
+        {
+            
+ 
+        }
+
+        private void main_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+         
+        }
+
+        private void btnStart_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.D)
+            {
+                main.Location = new Point((main.Location.X + vitessemain), main.Location.Y);
+            }
+            if (e.KeyCode == Keys.Q)
+            {
+                main.Location = new Point((main.Location.X - vitessemain), main.Location.Y);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void balle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
